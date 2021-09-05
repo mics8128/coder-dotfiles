909e4318227ad5eb642e14ba9f6033ed5aa8491c
Mics <mics@estiginto.com>
2021-09-05 06:33:58 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# 主要目標
+
+* 給 coder 使用的 :)
+* 安裝 yadm 並初始化我的 dotfiles
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+BIN_PATH=$HOME/.local/bin
+
+# 設定 安裝時的 PATH
+mkdir -p $BIN_PATH
+PATH=~/.local/bin:$PATH
+
+# 安裝 yadm (sh)
+if [[ ! -f $(which yadm) ]]; then
+    echo installing yadm...
+    curl -sfLo $BIN_PATH/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x $BIN_PATH/yadm > /dev/null
+else
+    echo yadm already installed
+fi
+
+yadm clone git@github.com:mics8128/dotfiles-yadm.git
+yadm bootstrap
